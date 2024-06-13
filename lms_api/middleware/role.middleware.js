6bffefd428e4c49d4905a90b7e8e64fd03b4df58
Surendra Singh <132122228+SursaChauhan@users.noreply.github.com>
2024-06-14 02:11:09 +0530

    Delete lms_api directory

--- lms_api/middleware/role.middleware.js
+++
@@ -1,7 +0,0 @@
-const isTeacher = (req, res, next) => {
-    if (req.user.roleId !== 1) return res.sendStatus(403); // Assuming role_id 1 is for teachers
-    next();
-  };
-  
-  module.exports = isTeacher;
-  
